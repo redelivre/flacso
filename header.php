3609ab9e4318952fa1b0fb0de9845bce7b7c0e36
Eduardo Zulian <eduardozulian@gmail.com>
2014-11-18 17:57:57 -0200

    Add new layout styles

--- header.php
+++ header.php
@@ -20,18 +20,27 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site container">
+
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'flacso' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
+		<div class="row">
+			<div class="site-complementary clear">
+				<div class="col-md-6">
+					<nav id="site-navigation--top" class="site-navigation top-navigation" role="navigation">
+						<?php wp_nav_menu( array( 'theme_location' => 'top' ) ); ?>
+					</nav><!-- #site-navigation -->
+				</div>
+				<div class="col-md-6">
+					<?php get_search_form(); ?>
+				</div>
+			</div><!-- .site-complementary -->
+		</div>
+
 		<div class="site-branding">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'flacso' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
