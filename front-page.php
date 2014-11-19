c55876b7440d1a0d3146c00d7de11b620fc37937
Eduardo Zulian <eduardozulian@gmail.com>
2014-11-19 10:05:34 -0200

    Add news section

--- front-page.php
+++ front-page.php
@@ -27,6 +27,44 @@ get_header(); ?>
 		<div class="col-md-12">
 			<h3>Notícias</h3>
 		</div>
+			<?php
+			$noticias = new WP_Query( array ( 'posts_per_page' => 3, 'ignore_sticky_posts' => true ) );
+
+			if ( $noticias->have_posts() ) : while ( $noticias->have_posts() ) : $noticias->the_post(); ?>
+
+				<div class="col-md-4">
+
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<header class="entry-header">
+							<div class="entry-image">
+								<a href="<?php the_permalink(); ?>" rel="bookmark">
+									<?php
+									if ( has_post_thumbnail() ) :
+										the_post_thumbnail( 'large' );
+									else :
+										echo '<img src="http://placehold.it/350x262/">';
+									endif;
+									?>
+								</a>
+							</div><!-- .entry-image -->
+							
+							<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+							<?php if ( 'post' == get_post_type() ) : ?>
+							<div class="entry-meta">
+								<?php flacso_posted_on(); ?>
+							</div><!-- .entry-meta -->
+							<?php endif; ?>
+						</header><!-- .entry-header -->
+
+						<div class="entry-content entry-content--summary">
+							<?php the_excerpt(); ?>
+						</div><!-- .entry-content -->
+					</article><!-- #post-## -->
+				</div>
+
+			<?php endwhile; endif;
+
+			?>
 	</div><!-- .row -->
 
 	<div class="row">
