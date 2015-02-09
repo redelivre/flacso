65cbdac6cb4cf0efae64a1c46e187cde06011230
Eduardo Zulian <eduardozulian@gmail.com>
2015-02-09 12:05:16 -0200

    Add category meta for regular posts

--- front-page.php
+++ front-page.php
@@ -129,7 +129,7 @@ get_header(); ?>
 									<?php endif; ?>
 
 									<div class="entry-meta">
-										<?php the_category(', '); ?>
+										<?php the_category(' '); ?>
 									</div><!-- .entry-meta -->
 									
 									<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
