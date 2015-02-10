d224a3d5adf5149afe4b35e4252d85fc98fe2fbf
Eduardo Zulian <eduardozulian@gmail.com>
2015-02-10 21:04:21 -0200

    Display projects relationship with Program pages
    Fix #74

--- content-page.php
+++ content-page.php
@@ -19,6 +19,53 @@
 				'after'  => '</div>',
 			) );
 		?>
+
+		<?php
+		/*
+		 * If is child of our page template that serves Programs, display the
+		 * related projects
+		 */
+		$parent_page_template = get_post_meta( $post->post_parent, '_wp_page_template', true );
+
+		if ( $parent_page_template == 'page-templates/child-page-list.php' ) :
+
+			// Loop through the terms
+			$statuses = get_terms( 'status', array( 'hide_empty' => true ) );
+
+			foreach ( $statuses as $status ) {
+
+				$args = array (
+					'post_type' => 'project',
+					'posts_per_page' => -1,
+					'meta_key'   => '_flacso-program-relation',
+					'meta_value' => $post->ID,
+					'tax_query' => array(
+						array(
+							'taxonomy' => 'status',
+							'field'    => 'id',
+							'terms'    => array( $status->term_id ),
+						)
+					)
+				);
+			
+				$projects = new WP_Query( $args );
+
+				if ( $projects->have_posts() ) : ?>
+
+					<h3><?php echo $status->name; ?></h3>
+					<ul>
+						<?php while ( $projects->have_posts() ) : $projects->the_post(); ?>
+							<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><?php edit_post_link( __( 'Editar projeto', 'flacso' ), '&nbsp;<span class="edit-link">', '</span>' ); ?></li>
+						<?php endwhile; ?>
+					</ul>
+				<?php
+				endif;
+
+				wp_reset_postdata();
+			}
+
+		endif;
+		?>
 	</div><!-- .entry-content -->
 	
 	<?php edit_post_link( __( 'Edit', 'flacso' ), '<footer class="entry-footer"><span class="edit-link">', '</span></footer>' ); ?>
