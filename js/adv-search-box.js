e0437b28568cfe8f033d374bd4474d60cffa29e8
Jacson Passold <jacson@ethymos.com.br>
2015-03-20 13:30:05 -0300

    scrollto main

--- js/adv-search-box.js
+++ js/adv-search-box.js
@@ -21,7 +21,7 @@ function flacso_adv_search_click()
 			paged: jQuery('input[name="adv-search-paged"]').val()
 		},
 		success: function(response) {
-			jQuery("html, body").animate({ scrollTop: 0 }, "slow");
+			jQuery("html, body").animate({ scrollTop: jQuery('#main').first().offset().top }, "slow");
 			jQuery('.general-list').replaceWith(response);
 			flacso_adv_search_result();
 			jQuery('.icon-spin6.animate-spin.icon--large').addClass('hidden')
