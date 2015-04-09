6e10bb90271cd7a25ea1ecf2d8850e581b99f543
Jacson Passold <jacson@ethymos.com.br>
2015-04-09 14:29:27 -0300

    need the div to ajax update

--- page-templates/library.php
+++ page-templates/library.php
@@ -35,76 +35,75 @@ get_header(); ?>
 				</article>
 
 				<?php endwhile; ?>
-
-				<?php
-				if(get_query_var('publication-type', false) || get_query_var('cat', false) || get_query_var('tag', false))
-				{
-					$tax_query = '"", ""';
-					if(get_query_var('publication-type', false))
+				<div class="general-list">
+					<?php
+					if(get_query_var('publication-type', false) || get_query_var('cat', false) || get_query_var('tag', false))
 					{
-						$tax_query = '"publication-type", '.get_query_var('publication-type', false);
-					}
-					elseif(get_query_var('cat', false)) 
-					{
-						$tax_query = '"cat", '.get_query_var('cat', false);
+						$tax_query = '"", ""';
+						if(get_query_var('publication-type', false))
+						{
+							$tax_query = '"publication-type", '.get_query_var('publication-type', false);
+						}
+						elseif(get_query_var('cat', false)) 
+						{
+							$tax_query = '"cat", '.get_query_var('cat', false);
+						}
+						else 
+						{
+							$tax_query = '"post_tag", '.get_query_var('tag', false);
+						}
+						?>
+	
+						<script type="text/javascript">
+						<!--
+							jQuery(document).ready(function () {
+								flacso_tax_click(<?php echo $tax_query; ?>);
+							});
+						//-->
+						</script>
+						<?php
 					}
 					else 
 					{
-						$tax_query = '"post_tag", '.get_query_var('tag', false);
-					}
-					?>
-
-					<script type="text/javascript">
-					<!--
-						jQuery(document).ready(function () {
-							flacso_tax_click(<?php echo $tax_query; ?>);
-						});
-					//-->
-					</script>
-					<?php
-				}
-				else 
-				{
-					$args = array(
-						'orderby' 	=> 'name',
-						'order' 	=> 'ASC',
-						'parent' 	=> 0,
-					);
-	
-					$publications = get_terms( 'publication-type', $args );
-					?>
-					<?php if ( $publications ) : ?>
-					<div class="general-list">
-						<?php
-						foreach( $publications as $publication ) :
-							$publication_link = get_term_link( $publication );
-							if( is_gea() )
-							{
-								$publication_link .= (strpos($publication_link, '?') ? '&' : '?').'gea=GEA';
-							}
+						$args = array(
+							'orderby' 	=> 'name',
+							'order' 	=> 'ASC',
+							'parent' 	=> 0,
+						);
+		
+						$publications = get_terms( 'publication-type', $args );
 						?>
-						<div class="general-list__item media clear">
-							<div class="pull-left">
-								<a href="<?php echo 'javascript:flacso_tax_click(\'publication-type\', '.$publication->term_id.');' ?>" class="icon icon--large icon--rounded">
-						    	<?php
-						    		if ( function_exists( 'get_tax_meta' ) ) {
-						    			echo '<span class="' . get_tax_meta( $publication->term_id, 'flacso_icon_picker' ) . '"></span>';
-						    		}
-						    	?>
-						    	</a>
-							</div>
-							<div class="media-body">
-							    <h1 class="general-list__title page-title media-heading"><a href="<?php echo 'javascript:flacso_tax_click(\'publication-type\', '.$publication->term_id.');'; ?>"><?php echo $publication->name; ?></a></h1>
-							    <?php if ( ! empty( $publication->description ) ) : ?>
-							    <div class="taxonomy-description"><?php echo $publication->description; ?></div>
-								<?php endif; ?>
-							    <a href="<?php echo 'javascript:flacso_tax_click(\'publication-type\', '.$publication->term_id.');'; ?>" class="read-more pull-right"><?php _e( 'Read more', 'flacso' ); ?><span class="more-sign">+</span></a>
-							</div><!-- .media-body -->
-					    </div><!-- .general-list__item.media -->
-					    <?php endforeach; ?>
-					</div><!-- .general-list -->
-					<?php endif; 
-				}?>
+						<?php if ( $publications ) : ?>
+							<?php
+							foreach( $publications as $publication ) :
+								$publication_link = get_term_link( $publication );
+								if( is_gea() )
+								{
+									$publication_link .= (strpos($publication_link, '?') ? '&' : '?').'gea=GEA';
+								}
+							?>
+							<div class="general-list__item media clear">
+								<div class="pull-left">
+									<a href="<?php echo 'javascript:flacso_tax_click(\'publication-type\', '.$publication->term_id.');' ?>" class="icon icon--large icon--rounded">
+							    	<?php
+							    		if ( function_exists( 'get_tax_meta' ) ) {
+							    			echo '<span class="' . get_tax_meta( $publication->term_id, 'flacso_icon_picker' ) . '"></span>';
+							    		}
+							    	?>
+							    	</a>
+								</div>
+								<div class="media-body">
+								    <h1 class="general-list__title page-title media-heading"><a href="<?php echo 'javascript:flacso_tax_click(\'publication-type\', '.$publication->term_id.');'; ?>"><?php echo $publication->name; ?></a></h1>
+								    <?php if ( ! empty( $publication->description ) ) : ?>
+								    <div class="taxonomy-description"><?php echo $publication->description; ?></div>
+									<?php endif; ?>
+								    <a href="<?php echo 'javascript:flacso_tax_click(\'publication-type\', '.$publication->term_id.');'; ?>" class="read-more pull-right"><?php _e( 'Read more', 'flacso' ); ?><span class="more-sign">+</span></a>
+								</div><!-- .media-body -->
+						    </div><!-- .general-list__item.media -->
+						    <?php endforeach; ?>
+						<?php endif; 
+					}?>
+				</div><!-- .general-list -->
 			</main><!-- #main -->
 		</div><!-- #primary -->
 	</div><!-- .col-md-# -->
